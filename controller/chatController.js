0ae36607da18ae58aa298808cea2cd244e2e3233
“Suvadeep” <Suvadeep@zohomail.in>
2023-12-19 14:25:00 +0530

    Send message controller and frontend

--- controller/chatController.js
+++ controller/chatController.js
@@ -1,19 +1,29 @@
 const Chat = require("../models/chatModel");
+const Group = require("../models/groupModel");
 
 exports.sendMessage = async (req, res, next) => {
   try {
+    //extract groupName and message
+    const groupName = req.body.groupName;
+    const message = req.body.message;
+
+    //extract message from auth.js
+    const user = req.user.name;
+
     const group = await Group.findOne({
-      where: { name: req.body.groupName },
+      where: { name: groupName },
     });
-    const createdMessage = await Chat.create({
-      name: req.user.name,
-      message: req.body.message,
+
+    const groupId = group.dataValues.id;
+
+    const chatDB = await Chat.create({
+      name: user,
+      message: message,
       userId: req.user.id,
       groupId: group.dataValues.id,
     });
     return res.status(200).json({ message: "Success!" });
   } catch (err) {
-    console.log("Error in send Message controller", err);
-    return res.status(500).json({ error: "Internal Server Error" });
+    console.log(err);
   }
 };
