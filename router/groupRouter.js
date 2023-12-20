2b92e8431af2da4be884ce22637d3b3686ca356a
“Suvadeep” <Suvadeep@zohomail.in>
2023-12-20 18:24:39 +0530

    Added comments to the routes

--- router/groupRouter.js
+++ router/groupRouter.js
@@ -6,6 +6,8 @@ const userAuthentication = require("../middleware/auth");
 router.post("/createGroup", userAuthentication, groupController.createGroup);
 router.get("/getGroups", userAuthentication, groupController.getGroups);
 router.post("/addToGroup", userAuthentication, groupController.addTogroup);
+//Including :id in the route path allows these routes to capture the  ID from the URL.
+//This ID is then used to identify the specific expense to be deleted or edited.
 router.post(
   "/deleteFromGroup",
   userAuthentication,
